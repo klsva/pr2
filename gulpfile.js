e927d2362ea93b6b3f2ede03d40edef80bdd98c2
SergeyGG <1letterssk@gmail.com>
2017-03-26 19:32:49 +0400

    hw2

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var googlecdn = require('gulp-google-cdn');
+var sass = require('gulp-sass');
+var clean = require('gulp-clean');
+ 
+gulp.task('goocdn', function (done) {
+    return gulp.src('./index.html')
+        .pipe(googlecdn(require('./bower.json'), {
+        	componentsPath: './bower_components/'
+        }))
+        .pipe(gulp.dest('./dist'));
+        done();
+});
+
+gulp.task('clean', function(done) {
+    return gulp.src('./dist', {read: false})
+        .pipe(clean());
+    done();
+});
+
+gulp.task('scss', gulp.series('clean', function(done) {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass({
+    	outputStyle: 'expanded'
+  }).on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+    done();
+}));
+
+gulp.task('scss:watch', function() {
+  gulp.watch('./scss/**/*.scss', gulp.series('scss', 'goocdn'));
+});
